a1a85cb7554b3553a812638e6d3a6514ed1d9adf
rubenbot07 <rubenbot77@hotmail.com>
2025-08-15 11:13:15 -0500

    changing localhost in sing in with google by env.NEXT_PUBLIC_SITE_URL

--- actions/sign-in-google.js
+++ actions/sign-in-google.js
@@ -1,24 +1,24 @@
 'use server'
 import { createSupabaseServerClient } from '@/lib/supabase/server'
-import { redirect} from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 const signInWith = provider => async () => {
-    const supabase =  createSupabaseServerClient()
-    const auth_callbackUrl = 'http://localhost:3000/auth/callback'
+  const supabase = createSupabaseServerClient()
+  const auth_callbackUrl = `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`
 
-    const { data, error } = await supabase.auth.signInWithOAuth({
+  const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
     options: {
-        redirectTo: auth_callbackUrl,
+      redirectTo: auth_callbackUrl,
     },
-    })
+  })
 
-    console.log('signInWith', provider, data)
-    if (error) {
-        console.error('Error signing in with', provider, error)
-        throw error
-    }
-    redirect(data.url || '/')
+  console.log('signInWith', provider, data)
+  if (error) {
+    console.error('Error signing in with', provider, error)
+    throw error
+  }
+  redirect(data.url || '/')
 }
 
 const signInWithGoogle = signInWith('google')
